64c695a3da13ebd1ffda047ba11831d3692d4d31
maxHarsh <116560167+maxHarsh@users.noreply.github.com>
2024-03-08 00:15:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taking input/taking-input.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void inputData() {
+        int a;
+        string s;
+        cin>>a;
+        cin>>s;
+        cout<<a<<" "<<s<<endl;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        Solution obj;
+        
+        obj.inputData();
+        
+    }
+}
+
+// } Driver Code Ends
