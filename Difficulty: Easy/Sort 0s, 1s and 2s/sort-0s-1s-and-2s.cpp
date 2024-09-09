f5ad83d62a7d5b733a6b71e17a34876670573c54
Harsh Dogney <116560167+Harsh-Dogney@users.noreply.github.com>
2024-09-09 19:15:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -7,8 +7,25 @@ using namespace std;
 class Solution {
   public:
     void sort012(vector<int>& arr) {
-        // code here
-        sort(arr.begin(),arr.end());
+    int n = arr.size();
+    int low =0,mid=0,high =n-1;
+    for(int i =0;i<n;i++)
+    {
+        if(arr[mid] == 0)
+        {
+            swap(arr[low],arr[mid]);
+            mid++;
+            low ++;        }
+        else if(arr[mid] ==1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
     }
 };
 
